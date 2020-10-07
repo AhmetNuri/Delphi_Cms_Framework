eb65a70a109c8c2ab2bcb8ebb74dbacef839537a
AhmetNuri <listeler@hotmail.com>
2020-10-07 21:24:21 +0300

    no message

--- uMain.pas
+++ uMain.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
   System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, RzCommon, RzTabs,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus,
   Vcl.ComCtrls, Vcl.ExtCtrls, uDmodMain, uTemlate1;
 
 type
